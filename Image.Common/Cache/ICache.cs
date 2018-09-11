cf2a065cb8dfe694aab7575acd3e9a3cd5d1f3ca
qin <248243224@qq.com>
2018-09-11 18:01:33 +0800

    update

---
+++ Image.Common/Cache/ICache.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Image.Common.Cache
+{
+    public interface ICache
+    {
+        void Set(string key, object value);
+        T Get<T>(string key);
+    }
+}
